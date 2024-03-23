41eb8dbee88411bf50d7d04b94bcd2b58254d9e8
otman oulcaid <57399454+otmanoulcaid@users.noreply.github.com>
2024-03-23 03:13:50 +0000

    removing unused comment

--- MachineLearning/models.py
+++ MachineLearning/models.py
@@ -42,35 +42,3 @@ class   Models(Operations):
                 "MLPClassifier" : MLPClassifier()
             }
 
-    
-
-
-# #loop through all the the algorithms and save the result into an array list of dictionary
-# result_list=[]
-
-# for model_name, model in classification_models.items():
-#         # Fit the model on the training data
-#         model.fit(X, y_encoded)
-
-#         # Predict on both training and validation sets
-#         y_train_pred = model.predict(X)
-#         y_valid_pred = model.predict(X_test)
-#         print(X_test)
-#         print(y_valid_pred)
-
-#         # Calculate accuracy scores
-#         train_accuracy = accuracy_score(y_encoded, y_train_pred)
-#         valid_accuracy = accuracy_score(y_testencoded, y_valid_pred)
-        
-
-#         # Save the results in the list
-#         result_list.append({
-#             'Model': model_name,
-#             'Training Accuracy': train_accuracy,
-#             'Validation Accuracy': valid_accuracy})
-
-
-# # Convert the list of dictionaries into a DataFrame
-# result_df = pd.DataFrame(result_list)
-
-# display(result_df)
