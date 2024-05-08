fc66ef170d133ea85656cc6eb6f9460aed84e178
Otman Oulcaid <ooulcaid@e2r3p9.1337.ma>
2024-05-08 20:03:32 +0100

    filtred data enpoint

--- urls.py
+++ urls.py
@@ -16,11 +16,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from .views import predict, getSymptoms, getDescription
+from .views import predict, getSymptoms, getDescription, getfiltredData
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('predict/', predict),
     path('getSymptoms/', getSymptoms),
+    path('getFiltredData/', getfiltredData)
     path('getDescription/<str:ds>/', getDescription)
 ]
--- views.py
+++ views.py
@@ -1,14 +1,8 @@
 import json
 from django.http import JsonResponse
-<<<<<<< HEAD
 from django.views.decorators.csrf import csrf_exempt
 from MachineLearning.predict import trained_model_prediction, symptoms, Description
 
-@csrf_exempt
-=======
-from MachineLearning.predict import trained_model_prediction, symptoms, Description
-
->>>>>>> 4f30895cda2e02d6f2ee3df84ef9e5dc19a7d47f
 def predict(request):
     if request.method == 'POST':
         data_json_str = request.body.decode('utf-8')
@@ -16,19 +10,11 @@ def predict(request):
         data_list = json.loads(data_json_str)
         prediction = trained_model_prediction(data_list) #call the predictif model
         object_result = {
-<<<<<<< HEAD
             'condition' : prediction[0],
             'description' : Description(prediction[0])[0]
         }
         return JsonResponse(object_result, safe=False)
     return JsonResponse(['Only POST requests are allowed'], safe=False)
-=======
-            'condition' : prediction,
-            'description' : Description(prediction)
-        }
-        return JsonResponse(object_result, safe=False)
-    return JsonResponse(['Only POST requests are allowed'])
->>>>>>> 4f30895cda2e02d6f2ee3df84ef9e5dc19a7d47f
 
 @csrf_exempt
 def getSymptoms(request):
@@ -37,14 +23,22 @@ def getSymptoms(request):
         return JsonResponse(result, safe=False)
     return JsonResponse(['Only GET requests are allowed'])
 
-<<<<<<< HEAD
 @csrf_exempt
-=======
->>>>>>> 4f30895cda2e02d6f2ee3df84ef9e5dc19a7d47f
 def getDescription(request, ds):
     if ds is None:
         return JsonResponse(['you should pass a condition as an argument'])
     if request.method == 'GET':
         result = Description(ds)
+        return JsonResponse(result, safe=False)
+    return JsonResponse(['Only POST requests are allowed'])
+
+@csrf_exempt
+def getfiltredData(request):
+    if request.method == 'GET':
+        data = {
+            'skin' : ['nta', 'ras', 'lgr3a'],
+            'exemple' : ['khod', 'hadi', 'exemple']
+            'php' : ['MVC', 'login', 'les regle']
+        }
         return JsonResponse(result, safe=False)
     return JsonResponse(['Only POST requests are allowed'])
